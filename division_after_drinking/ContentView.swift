5aba944c9296cd300fbd82690b2c4a7840a3b5ac
Fedorov Vladimir <vladimir.fodorow@gmail.com>
2022-07-03 22:27:06 +0700

    Initial Commit

---
+++ division_after_drinking/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  division_after_drinking
+//
+//  Created by Владимир Федоров on 03.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ division_after_drinking/division_after_drinkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  division_after_drinkingApp.swift
+//  division_after_drinking
+//
+//  Created by Владимир Федоров on 03.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct division_after_drinkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
