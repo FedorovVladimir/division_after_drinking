9bb21cd0c5825c4f00a1343cc0740e1ad1d67052
Fedorov Vladimir <vladimir.fodorow@gmail.com>
2022-07-12 02:07:19 +0700

    add for iphone

--- division_after_drinking/ContentView.swift
+++ division_after_drinking/ContentView.swift
@@ -31,90 +31,129 @@ struct ContentView: View {
     @State private var showAddOutcomeModal: Bool = false
 
     var body: some View {
-        HStack(spacing: 0) {
-            List {
-                Section(header: Text("Участники попоя")) {
-                    ForEach(payersModel.data.payers, id: \.id) { item in
-                        PayerView(payer: item)
-                                .swipeActions {
-                                    Button {
-                                        for i in 0..<payersModel.data.payers.count {
-                                            if payersModel.data.payers[i].id == item.id {
-                                                payersModel.data.payers.remove(at: i)
-                                                break
-                                            }
-                                        }
-                                        payersModel.calc()
-                                    } label: {
-                                        Label("Удалить", systemImage: "trash")
-                                    }
-                                            .tint(.red)
-                                }
-                    }
-                            .listStyle(.sidebar)
-                    Button(action: {
-                        self.showAddPayerModal = true
-                    }) {
-                        HStack {
-                            Text("Добавить")
-                            Spacer()
-                            Text(String(format: "%.2f", Float64(payersModel.data.payersSum) / 100))
-                        }
-                    }
-                            .sheet(isPresented: self.$showAddPayerModal) {
-                                AddPayerModalView()
-                                        .environmentObject(payersModel)
-                            }
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            VStack(spacing: 0) {
+                List {
+                    PayerSection(payersModel: payersModel)
+                    OutcomeSection(payersModel: payersModel)
                 }
             }
-                    .frame(height: .infinity)
-            List {
-                Section(header: Text("Чек")) {
-                    ForEach(payersModel.data.outcomes, id: \.id) { item in
-                        OutcomeView(outcome: item, payers: payersModel.data.payers)
-                                .swipeActions {
-                                    Button {
-                                        for i in 0..<payersModel.data.outcomes.count {
-                                            if payersModel.data.outcomes[i].id == item.id {
-                                                payersModel.data.outcomes.remove(at: i)
-                                                break
-                                            }
-                                        }
-                                        payersModel.calc()
-                                    } label: {
-                                        Label("Удалить", systemImage: "trash")
-                                    }
-                                            .tint(.red)
-                                }
-                    }
-                            .listStyle(.sidebar)
-                    Button(action: {
-                        self.showAddOutcomeModal = true
-                    }) {
-                        HStack {
-                            Text("Добавить")
-                            Spacer()
-                            Text(String(format: "%.2f", Float64(payersModel.data.outcomesSum) / 100))
-                        }
-                    }
-                            .sheet(isPresented: self.$showAddOutcomeModal) {
-                                AddOutcomeModalView()
-                                        .environmentObject(payersModel)
-                            }
+                    .onAppear(perform: onAppearMainView)
+        } else {
+            HStack(spacing: 0) {
+                List {
+                    PayerSection(payersModel: payersModel)
+                }
+                List {
+                    OutcomeSection(payersModel: payersModel)
                 }
             }
+                    .onAppear(perform: onAppearMainView)
         }
-                .onAppear {
-//                    payersModel.data.payers += fakePayers
-//                    payersModel.data.outcomes += fakeOutcomes
-                    payersModel.calc()
+    }
+
+    func onAppearMainView() {
+//        payersModel.data.payers += fakePayers
+//        payersModel.data.outcomes += fakeOutcomes
+        payersModel.calc()
+    }
+}
+
+struct PayerSection: View {
+    @StateObject var payersModel: PayersModel
+    @State private var showAddPayerModal: Bool = false
+
+    var body: some View {
+        Section(header: Text("Участники попоя")) {
+            ForEach(payersModel.data.payers, id: \.id) { item in
+                PayerView(payer: item)
+                        .swipeActions {
+                            Button {
+                                for i in 0..<payersModel.data.payers.count {
+                                    if payersModel.data.payers[i].id == item.id {
+                                        payersModel.data.payers.remove(at: i)
+                                        break
+                                    }
+                                }
+                                payersModel.calc()
+                            } label: {
+                                Label("Удалить", systemImage: "trash")
+                            }
+                                    .tint(.red)
+                        }
+            }
+                    .listStyle(.sidebar)
+            Button(action: {
+                self.showAddPayerModal = true
+            }) {
+                HStack {
+                    Text("Добавить")
+                    Spacer()
+                    Text(String(format: "%.2f", Float64(payersModel.data.payersSum) / 100))
                 }
+            }
+                    .sheet(isPresented: self.$showAddPayerModal) {
+                        AddPayerModalView()
+                                .environmentObject(payersModel)
+                    }
+        }
+    }
+}
+
+struct OutcomeSection: View {
+    @StateObject var payersModel: PayersModel
+    @State private var showAddOutcomeModal: Bool = false
+
+    var body: some View {
+        Section(header: Text("Чек")) {
+            ForEach(payersModel.data.outcomes, id: \.id) { item in
+                OutcomeView(outcome: item, payers: payersModel.data.payers)
+                        .swipeActions {
+                            Button {
+                                for i in 0..<payersModel.data.outcomes.count {
+                                    if payersModel.data.outcomes[i].id == item.id {
+                                        payersModel.data.outcomes.remove(at: i)
+                                        break
+                                    }
+                                }
+                                payersModel.calc()
+                            } label: {
+                                Label("Удалить", systemImage: "trash")
+                            }
+                                    .tint(.red)
+                        }
+            }
+                    .listStyle(.sidebar)
+            Button(action: {
+                self.showAddOutcomeModal = true
+            }) {
+                HStack {
+                    Text("Добавить")
+                    Spacer()
+                    Text(String(format: "%.2f", Float64(payersModel.data.outcomesSum) / 100))
+                }
+            }
+                    .sheet(isPresented: self.$showAddOutcomeModal) {
+                        AddOutcomeModalView()
+                                .environmentObject(payersModel)
+                    }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+                .previewDevice("iPhone 8")
+                .previewInterfaceOrientation(.portrait)
+        ContentView()
+                .previewDevice("iPhone 8")
+                .previewInterfaceOrientation(.landscapeLeft)
+        ContentView()
+                .previewDevice("iPad Air (5th generation)")
+                .previewInterfaceOrientation(.portrait)
+        ContentView()
+                .previewDevice("iPad Air (5th generation)")
                 .previewInterfaceOrientation(.landscapeLeft)
     }
 }
